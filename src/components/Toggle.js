95d36bad9708f0bef5fe87eac952bd1be56f6c22
Irene <irene.muthomi@student.moringaschool.com>
2023-11-04 18:29:24 +0300

    completed assignment

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,4 +1,5 @@
 import React from "react";
+//import React, { useState } from "react";
 
 function Toggle() {
   return <button>OFF</button>;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-import React from "react";
+//import React from "react";
+//import ReactDOM from "react-dom";
+//import App from "./components/App";
+
+//ReactDOM.render(<App />, document.getElementById("root"));
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+function Toggle() {
+    const [isOn, setIsOn] = useState(false);
+  
+    function handleClick() {
+      setIsOn((isOn) => !isOn);
+    }
+  
+    const color = isOn ? "red" : "white";
+  
+    return (
+      <button style={{ background: color }} onClick={handleClick}>
+        {isOn ? "ON" : "OFF"}
+      </button>
+    );
+  }
+
